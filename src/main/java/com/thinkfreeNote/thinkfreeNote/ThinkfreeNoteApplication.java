07b7f7c6fa59f2a544cc334e1e077538821f3435
dnrgus1127 <dnrgus1127@naver.com>
2024-05-18 22:51:43 +0900

    first commit

---
+++ src/main/java/com/thinkfreeNote/thinkfreeNote/ThinkfreeNoteApplication.java
@@ -0,0 +1,13 @@
+package com.thinkfreeNote.thinkfreeNote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkfreeNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinkfreeNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thinkfreeNote/thinkfreeNote/ThinkfreeNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinkfreeNote.thinkfreeNote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkfreeNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

cc654fc9df5d93a4f20aa4d68f5857641155369f
dnrgus1127 <dnrgus1127@naver.com>
2024-05-27 15:57:40 +0900

    feat/block : 에디터 ref 핫픽스

--- src/frontend/src/component/editor/NoteEditor.jsx
+++ src/frontend/src/component/editor/NoteEditor.jsx
@@ -2,16 +2,15 @@ import React, {createContext, useRef} from 'react';
 import NoteBlockSwitcher from "./NoteBlockSwitcher";
 import useEditHandler from "./hooks/useEditHandler";
 
-const EditorContext = createContext(null);
+export const EditorContext = createContext(null);
 
 function NoteEditor({noteContents, setNoteContents}) {
     const {onKeyDownHandler} = useEditHandler(noteContents, setNoteContents);
     const editorRef = useRef(null);
-    const $editor = editorRef.current;
 
     return (
-        <EditorContext.Provider value={$editor}>
-            <div ref={editorRef} onKeyDown={onKeyDownHandler} contentEditable={true}
+        <EditorContext.Provider value={editorRef}>
+            <div className="editor" spellCheck={false} ref={editorRef} onKeyDown={onKeyDownHandler} contentEditable={true}
                  suppressContentEditableWarning={true}>
                 {noteContents.map(blockId => <NoteBlockSwitcher key={blockId} blockId={blockId}/>)}
             </div>

3d3aaaf1ef9a6de6cce8acbac3b6649aad795e67
dnrgus1127 <dnrgus1127@naver.com>
2024-05-27 13:07:55 +0900

    feat/block : 에디터 ref 임시 PR

--- src/frontend/src/component/editor/NoteEditor.jsx
+++ src/frontend/src/component/editor/NoteEditor.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, {createContext, useRef} from 'react';
 import NoteBlockSwitcher from "./NoteBlockSwitcher";
 import useEditHandler from "./hooks/useEditHandler";
 
+const EditorContext = createContext(null);
+
 function NoteEditor({noteContents, setNoteContents}) {
     const {onKeyDownHandler} = useEditHandler(noteContents, setNoteContents);
+    const editorRef = useRef(null);
+    const $editor = editorRef.current;
 
     return (
-        <div onKeyDown={onKeyDownHandler} contentEditable={true} suppressContentEditableWarning={true}>
-            {noteContents.map(blockId => <NoteBlockSwitcher key={blockId} blockId={blockId}/>)}
-        </div>
+        <EditorContext.Provider value={$editor}>
+            <div ref={editorRef} onKeyDown={onKeyDownHandler} contentEditable={true}
+                 suppressContentEditableWarning={true}>
+                {noteContents.map(blockId => <NoteBlockSwitcher key={blockId} blockId={blockId}/>)}
+            </div>
+        </EditorContext.Provider>
     );
 }
 
--- src/frontend/src/component/editor/TextBlock.jsx
+++ src/frontend/src/component/editor/TextBlock.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
 
 function TextBlock({blockId}) {
+    const data = {};
+    const abc = () =>{
+        return {
+            fontWeight : data.bold && 600,
+            fontStyle : data.italic || 300,
+        }
+    }
+
+
+
     return (
-        <p>&#xFEFF;</p>
+        <p style={{fontWeight: 600}}>&#xFEFF;</p>
     );
 }
 

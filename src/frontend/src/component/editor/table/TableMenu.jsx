b57ca9fb6b3882014265d3adcff754e3c41f60f1
dnrgus1127 <dnrgus1127@naver.com>
2024-05-30 14:20:03 +0900

    feat/table : 테이블 메뉴 구현

--- src/frontend/src/component/editor/table/TableMenu.jsx
+++ src/frontend/src/component/editor/table/TableMenu.jsx
@@ -1,59 +1,63 @@
-import React, {useContext} from 'react';
+import React from 'react';
 import {ContextMenu} from "../../common/ContextMenu";
-import {BlockIdContext} from "../BlockWrapper";
-import {useBlockData} from "../hooks/useBlockHooks";
 import useTable from "./hooks/useTable";
-import {TableRerenderContext} from "./TableComponent";
+import {useTableData} from "./hooks/useTableData";
 
+const convertTypeToText = (type)=>{
+    return  type === "row" ? "행" : "열";
+}
+
+const getTextByDirection = (type,direction) => {
+    if(type === "row") {
+        return direction === "prev" ? "위로" : "아래로";
+    }
+    else {
+        return direction === "next" ? "오른쪽에" : "왼쪽에"
+    }
+}
+
+/**
+ * @desc 테이블 메뉴
+ * @param {function} closeMenu
+ * @param {"row"|"column"} type
+ * @param {number} rowIdx
+ * @param {number} colIdx
+ * @returns {JSX.Element}
+ */
 function TableMenu({closeMenu, type, rowIdx, colIdx}) {
     const headerAble = rowIdx === 0 && colIdx === 0;
-    const typeText = type === "row" ? "행" : "열";
-    const blockId = useContext(BlockIdContext);
-    const tableData = useBlockData(blockId);
-    const {addRow,addColumn,removeRow} = useTable(tableData);
-    const reRenderTable = useContext(TableRerenderContext);
+    const tableData = useTableData();
+    const deleteAble = type === "row" ? tableData.getRowLength() > 1 : tableData.getColumnLength() > 1;
+    const {addRow, addColumn, removeRow, removeColumn} = useTable();
 
-    const insertNext = () => {
-        if(type === "row") {
-            addRow(rowIdx);
-            reRenderTable();
-        }
-        else if(type === "col") {
-            addColumn(colIdx);
-            reRenderTable();
-        }
+    // 다음에 삽입
+    const insertNextMenuHandler = () => {
+        type === "row" ? addRow(rowIdx) : addColumn(colIdx);
         closeMenu();
     }
-    const insertPrev = () => {
-        if(type === "row") {
-            addRow(rowIdx-1);
-            reRenderTable();
-        }
-        else if(type === "col") {
-            addColumn(colIdx-1);
-            reRenderTable();
-        }
+    // 이전에 삽입
+    const insertPrevMenuHandler = () => {
+        type === "row" ? addRow(rowIdx -1 ) : addColumn(colIdx -1);
         closeMenu();
     }
-
-    const remove = () => {
-        if(type === "row") {
-            removeRow(rowIdx);
-            reRenderTable();
-        }
+    // 삭제
+    const removeMenuHandler = () => {
+        type === "row" ?  removeRow(rowIdx) : removeColumn(colIdx);
+        closeMenu();
     }
 
     return (
         <ContextMenu closeMenu={closeMenu}>
             {headerAble && (
                 <>
-                    <ContextMenu.Toggle name={`헤더 ${typeText}`}/>
+                    <ContextMenu.Toggle name={`헤더 ${convertTypeToText(type)}`}/>
                     <ContextMenu.Divider/>
                 </>
             )}
-            <ContextMenu.Plain name={`이전에 ${typeText} 삽입`} handler={insertPrev}/>
-            <ContextMenu.Plain name={`다음에 ${typeText} 삽입`} handler={insertNext}/>
-            <ContextMenu.Plain name={`${typeText} 삭제`} handler={remove}/>
+            <ContextMenu.Plain name={`${getTextByDirection(type,"prev")} ${convertTypeToText(type)} 삽입`} handler={insertPrevMenuHandler}/>
+            <ContextMenu.Plain name={`${getTextByDirection(type,"next")} ${convertTypeToText(type)} 삽입`} handler={insertNextMenuHandler}/>
+            <ContextMenu.Plain disable={!deleteAble} name={`${convertTypeToText(type)} 삭제`} handler={removeMenuHandler}/>
+            <ContextMenu.Divider/>
             <ContextMenu.Plain name="계산 함수"/>
         </ContextMenu>
     )

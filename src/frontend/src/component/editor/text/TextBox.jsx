27868fc111d0ae6325adfc88562f407aa7eff4bb
LeeKangmin <22rkdmin@naver.com>
2024-06-01 01:03:23 -0700

    feat/textBox: textDecoration, italic 구현

--- src/frontend/src/component/editor/text/TextBox.jsx
+++ src/frontend/src/component/editor/text/TextBox.jsx
@@ -7,7 +7,13 @@ function TextBox({targetBlockId, targetTextId, onRefresh}) {
 
     return (
         <div contentEditable={false} style={{userSelect: "none"}}>
-            <button onClick={() => text.updateFontStyle("fontWeight", "bold", onRefresh)}>Bord</button>
+            <button onClick={() => text.updateFontStyle("color", "blue", onRefresh)}>blue</button>
+            <button onClick={() => text.updateFontStyle("fontSize", 15, onRefresh)}>size</button>
+            <button onClick={() => text.updateFontStyle("fontWeight", "bold", onRefresh)}>bold</button>
+            <button onClick={() => text.updateFontStyle("fontStyle", "italic", onRefresh)}>italic</button>
+            <button onClick={() => text.updateFontStyle("textDecoration", "underline", onRefresh)}>underline</button>
+            <button onClick={() => text.updateFontStyle("textDecoration", "line-through", onRefresh)}>line-through</button>
+            <button onClick={() => text.updateFontStyle("textDecoration", "underline", onRefresh)}>underline</button>
         </div>
     );
 }

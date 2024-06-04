f90deccdb6e23baf2fde471468d95483884ea297
dnrgus1127 <dnrgus1127@naver.com>
2024-06-04 15:40:32 +0900

    feat/table : 블록 공통 메뉴 수정

--- src/frontend/src/component/editor/block/BlockContextMenu.jsx
+++ src/frontend/src/component/editor/block/BlockContextMenu.jsx
@@ -6,12 +6,9 @@ function BlockContextMenu({closeMenu}) {
         <ContextMenu closeMenu={closeMenu}>
             <ContextMenu.DropDown name="전환">
                 <ContextMenu.Plain name="텍스트박스"/>
-                <ContextMenu.Plain name="테이블"/>
             </ContextMenu.DropDown>
             <ContextMenu.Divider/>
-            <ContextMenu.Plain name="복사"/>
-            <ContextMenu.Plain name="붙여넣기"/>
-            <ContextMenu.Divider/>
+            <ContextMenu.Plain name="복제"/>
             <ContextMenu.Plain name="삭제"/>
         </ContextMenu>
     );

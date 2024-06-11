16fce5cb827adac35d2b03daf3c7b6fa01cbb5d4
dnrgus1127 <dnrgus1127@naver.com>
2024-06-11 15:28:00 +0900

    feat/fix : list 핫픽스
    - 리스트 옵저버 등록

--- src/frontend/src/component/editor/list/ListBlock.jsx
+++ src/frontend/src/component/editor/list/ListBlock.jsx
@@ -4,15 +4,18 @@ import {useBlockData} from "../hooks/useBlockHooks";
 import {BlockContext} from "../BlockContextProvider";
 import TextWrapper from "../text/TextWrapper";
 import TextComponent from "../text/TextComponent";
+import useTextBlockObserver from "../text/hooks/useTextBlockObserver";
 
 
 function ListBlock({type}) {
     const {blockId} = useContext(BlockContext);
     const textBlock = useBlockData(blockId);
     const liValue = type === "ul" ? "• " : textBlock.olIdx + ". ";
+    const {ref} = useTextBlockObserver(blockId);
+
 
     return (
-        <p className="list" data-li-value={liValue} data-leaf="true">
+        <p ref={ref} className="list" data-li-value={liValue} data-leaf="true">
             <TextWrapper>
                     {textBlock.textIdList.map(textId => (
                         <TextComponent

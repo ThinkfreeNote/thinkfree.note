acc8c4f3fe46c813f9f9cd3b256b8d51d984dd23
dnrgus1127 <dnrgus1127@naver.com>
2024-06-13 19:11:18 +0900

    feat/fix : 타이틀 엔터 핫픽스
    리팩토링 과정에서 타이틀에서 엔터 치면 발생하는 문제 해결

--- src/frontend/src/component/editor/Title.jsx
+++ src/frontend/src/component/editor/Title.jsx
@@ -2,12 +2,13 @@ import React, {useContext, useEffect, useMemo, useRef, useState} from 'react';
 import {useBlockStore} from "./hooks/useBlockHooks";
 import {EditorContext} from "./NoteEditor";
 import {NoteTitleContext} from "./context/NoteTitleProvider";
+import {editorSelection} from "../../App";
 
 function Title() {
     const blockStore = useBlockStore();
     const titleRef = useRef(null);
     const editorRef = useContext(EditorContext);
-    const {title,setTitle} = useContext(NoteTitleContext);
+    const {title, setTitle} = useContext(NoteTitleContext);
     const [text, setText] = useState(null);
 
     useEffect(() => {
@@ -15,22 +16,30 @@ function Title() {
             setTitle(titleRef.current.textContent);
         }
         const $editor = editorRef.current;
-        if(!$editor) return;
-        $editor.addEventListener("input",titleHandler);
-        return () => {
-            $editor.removeEventListener("input",titleHandler);
+        if (!$editor) return;
+        const preventEnter = (e) => {
+            if (editorSelection.getBlockType() === "title")
+                e.key === "Enter" && e.preventDefault();
         }
-    }, [blockStore,titleRef,editorRef]);
+
+        $editor.addEventListener("keydown", preventEnter);
+
+        $editor.addEventListener("input", titleHandler);
+        return () => {
+            $editor.removeEventListener("keydown", preventEnter);
+            $editor.removeEventListener("input", titleHandler);
+        }
+    }, [blockStore, titleRef, editorRef]);
 
     // 최초에만 title 상태값에 의존하고 이후 렌더링에서는 title 상태값에 의존하지 않게 하기 위함
-    useEffect(()=>{
-        if(text === null && title !== null) {
+    useEffect(() => {
+        if (text === null && title !== null) {
             setText(title);
         }
-    },[title])
+    }, [title])
 
 
-    if(!blockStore) return null;
+    if (!blockStore) return null;
     return (
         <h1 ref={titleRef} data-block-type="title" className="title">
             {text}

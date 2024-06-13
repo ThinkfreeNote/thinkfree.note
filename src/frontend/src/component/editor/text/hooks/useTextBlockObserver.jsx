c99cd1fe5ec4e36456169bd74951a495f84f00d9
dnrgus1127 <dnrgus1127@naver.com>
2024-06-13 19:08:21 +0900

    feat/fix : 옵저버 미적용 이슈 해결

--- src/frontend/src/component/editor/text/hooks/useTextBlockObserver.jsx
+++ src/frontend/src/component/editor/text/hooks/useTextBlockObserver.jsx
@@ -1,25 +1,24 @@
-import {useEffect, useRef} from "react";
-import {useBlockStore} from "../../hooks/useBlockHooks";
+import {useEffect} from "react";
 
 
 // TODO 크로스 브라우징 확인 필요
-function useTextBlockObserver(blockId) {
-    const textBlockRef = useRef(null);
-    const blockStore = useBlockStore();
+function useTextBlockObserver(ref) {
+
 
     useEffect(()=> {
-        if(!textBlockRef.current) return;
-        const $textBlock = textBlockRef.current;
-
+        if(!ref.current) {
+            return;
+        }
+        const $textBlock = ref.current;
         // node가 추가되는지 감시하는 MutationObserver
         const observer = new MutationObserver((mutationList, observer) => {
             for ( const mutation of mutationList ) {
                 // childList : node 추가 삭제 되는 경우 && addedNodes로 추가인 경우로 조건 축소
                 if(mutation.type === "childList" && mutation.addedNodes.length > 0) {
                     const target = mutation.target;
-                    
+
                     // TODO 크로스 브라우징 이슈 발생할 여지 높음
-                    // 동일 아이디의 span태그가 contentediable에 의해서 작성되어 발생하는 문제 해결 
+                    // 동일 아이디의 span태그가 contentediable에 의해서 작성되어 발생하는 문제 해결
                     if(target.tagName === "FONT" || target.tagName === "I") {
                         const textId = target.closest("[data-text-id]").dataset.textId;
                         // 동일한 ID의 span 태그를 합쳐주는 동작 수행
@@ -52,11 +51,7 @@ function useTextBlockObserver(blockId) {
         return () => {
             observer.disconnect();
         }
-    },[])
-
-
-    return {ref : textBlockRef}
-
+    },[ref.current])
 }
 
 export default useTextBlockObserver;

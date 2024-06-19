d6b7d6651b8e836aba67d6cba4d694e15bb863a5
LeeKangMin <22rkdmin@thinkfree.com>
2024-06-19 10:52:31 +0900

    feat/list: 자식은 부모 기준 1depth 를 넘지 못하게 예외 처리

--- src/frontend/src/component/editor/context/NoteIndexListProvider.jsx
+++ src/frontend/src/component/editor/context/NoteIndexListProvider.jsx
@@ -42,8 +42,6 @@ export function useIndexList() {
         return indexList[blockIndex];
     }
 
-
-
     return {getOrder}
 }
 
--- src/frontend/src/component/editor/list/hooks/useListHandler.jsx
+++ src/frontend/src/component/editor/list/hooks/useListHandler.jsx
@@ -3,25 +3,33 @@ import {editorSelection} from "../../../../App";
 import {useContext} from "react";
 import {BlockReRenderContext} from "../../context/BlockReRenderContext";
 import {useIndexList} from "../../context/NoteIndexListProvider";
+import useBlockIdList from "../../hooks/useBlockIdList";
 
 function useListHandler() {
     const blockStore = useBlockStore();
     const {setReRenderTargetId} = useContext(BlockReRenderContext);
     const {getOrder} = useIndexList();
+    const {getPrevBlockId} = useBlockIdList();
+
 
     const changeDepth = () => {
         const blockId = editorSelection.startBlockId;
         const block = blockStore.getBlock(blockId);
 
         if (editorSelection.isCaret() && block.type === "ol" || block.type === "ul") {
-            // 첫 목록 안되게 하기
+            // 첫 목록인 경우 예외 처리
             const order = getOrder(blockId);
             if (order === 0) return;
 
-            // 최대 Depth 를 넘은 경우
+            // 최대 Depth 를 넘은 경우 예외 처리
             if (block.depth >= 3) return;
 
-            // TODO: 부모 depth 보다 크게 안됨
+            // 자식은 부모 기준 1 depth 를 넘을 수 없음
+            const prevBlock = blockStore.getBlock(getPrevBlockId(blockId));
+            if (block.depth > prevBlock.depth) {
+                return;
+            }
+
 
             // TODO: Child 로 옮겨줌
 
